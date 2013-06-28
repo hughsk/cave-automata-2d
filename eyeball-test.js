7250d98aeaff1357ac051ebb599ea8f0bc0f6d0e
Hugh Kennedy <hughskennedy@gmail.com>
2013-06-29 09:12:02 +1000

    Use ndarray-stencil - closes #1.
    As a consequence, the elements in the outer border of
    the grid will always be set to 0. This can be worked
    around by using `hi` and `lo` on the grid after
    iterating.
    
    Also replacing "ndarray" module with "zeros".

--- eyeball-test.js
+++ eyeball-test.js
@@ -1,16 +1,16 @@
 var automata = require('./')
-var ndarray = require('ndarray')
+var zeros = require('zeros')
 
-var buffer = ndarray.zeros([120, 40])
+var buffer = zeros([120, 40])
   , width = buffer.shape[0]
   , height = buffer.shape[1]
   , iterate = automata(buffer, {
-    density: 0.465
+    density: 0.5
   })
 
 for (var i = 0; i < 10; i += 1) setTimeout(function() {
-    for (var y = 0; y < height; y += 1) {
-  for (var x = 0; x < width; x += 1) {
+  for (var y = 1; y < height-1; y += 1) {
+    for (var x = 1; x < width-1; x += 1) {
       process.stdout.write((buffer.get(x, y)) ? '@' : '.')
     }
     process.stdout.write('\n')
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var fill = require('ndarray-fill')
-var ndarray = require('ndarray')
 var neighborhood = require('moore')(1, 2).concat([[0,0]])
+var stencil = require('ndarray-stencil')
+var fill = require('ndarray-fill')
+var zero = require('zeros')
+
 var neighbors = neighborhood.length
 
 module.exports = generate
@@ -9,7 +11,7 @@ function generate(array, opts) {
 
   var width = array.shape[0]
     , height = array.shape[1]
-    , buffer = ndarray.zeros([ width, height ])
+    , buffer = zero([ width, height ])
 
   var density = opts.density || 0.5
     , threshold = opts.threshold || 5
@@ -18,42 +20,20 @@ function generate(array, opts) {
 
   if (shouldFill) fill(array, function(x, y) {
     return Math.random() <= density || (
-      x <= 0 || x >= width-1 ||
-      y <= 0 || y >= height-1
+      x <= 1 || x >= width-2 ||
+      y <= 1 || y >= height-2
     ) ? 1 : 0
   })
 
-  function iterate(inputBuffer, outputBuffer) {
-    var input = inputBuffer.data
-      , output = outputBuffer.data
-      , width = inputBuffer.shape[0]
-      , height = inputBuffer.shape[1]
-      , length = input.length
-      , n = 0, i = 0, m = 0
-      , x, y, nx, ny
-
-    for (x = 0; x < width; x += 1)
-    for (y = 0; y < height; y += 1) {
-      n = 0
-
-      for (m = 0; m < neighbors; m += 1) {
-        nx = neighborhood[m][0]
-        ny = neighborhood[m][1]
-
-        n += input[i+ny+nx*height] || (
-          x + nx < 0 ||
-          x + nx >= width ||
-          y + ny < 0 ||
-          y + ny >= height
-        ) ? 1 : 0
-      }
-
-      output[i] = n >= threshold ? 1 : 0
-      i += 1
-    }
-
-    return outputBuffer
-  }
+  var iterate = stencil(neighborhood, function(p1,p2,p3,p4,p5,p6,p7,p8,p9,pos) {
+    return (
+      p1+p2+p3+p4+p5+p6+p7+p8+p9 >= threshold ||
+      pos[0] <= 1 ||
+      pos[0] >= width-3 ||
+      pos[1] <= 1 ||
+      pos[1] >= height-3
+    ) ? 1 : 0
+  })
 
   if (opts.iterations) {
     iterator(opts.iterations)
@@ -66,9 +46,9 @@ function generate(array, opts) {
 
     for (var i = 0; i < iterations; i += 1) {
       if (i % 2) {
-        iterate(buffer, array)
-      } else {
         iterate(array, buffer)
+      } else {
+        iterate(buffer, array)
       }
     }
 
